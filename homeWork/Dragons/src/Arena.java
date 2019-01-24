c11f38de2cad93ae7653062721f71e2fb888e22a
RuslanMck <mckruslan@gmail.com>
2019-01-24 21:08:30 +0200

    Optimize  Arena

--- homeWork/Dragons/src/Arena.java
+++ homeWork/Dragons/src/Arena.java
@@ -1,42 +1,50 @@
 import model.Dragon;
 
 public class Arena {
-    public void fight(Dragon fstDng, Dragon scndDgn){
+
+    public void fight(Dragon fstDng, Dragon scndDgn) {
         int rounds = 0;
-        countBonus(fstDng,scndDgn);
-        while (fstDng.getHealth() >  0 && scndDgn.getHealth() > 0){
-
-            fstDng.setHealth(fstDng.getHealth() - (scndDgn.getAttack() * scndDgn.getBonusPower()));
-            scndDgn.setHealth(scndDgn.getHealth() - (fstDng.getAttack() * fstDng.getBonusPower()));
-
+        countBonus(fstDng, scndDgn);
+        while (fstDng.getHealth() > 0 && scndDgn.getHealth() > 0 && rounds < 10) {
             rounds++;
-            if (fstDng.getHealth() <= 0){
-                System.out.println(scndDgn.getName() + " won, " + fstDng.getName() + " lost in " + rounds + " round.");
-            } else if (scndDgn.getHealth() <= 0) {
-                System.out.println(fstDng.getName() + " won, " + scndDgn.getName() + " lost in " + rounds + " round.");
-            } else if (fstDng.getHealth() < 0 && scndDgn.getHealth() < 0){
-                System.out.println("Oops, we have a dead heat.");
+            countHealth(fstDng, scndDgn);
+            searchWinner(fstDng, scndDgn, rounds);
+        }
+    }
+
+    public void countBonus(Dragon dragonA, Dragon dragonB) {
+        int tmpPower = dragonA.getPower() ^ dragonB.getPower();
+        int complimentTmpPower = ~tmpPower;
+        int dragon2Multiplier = complimentTmpPower | dragonA.getPower();
+        int dragon1Multiplier = complimentTmpPower | dragonB.getPower();
+        int complimentDragon2Multiplier = ~dragon2Multiplier;
+        int complimentDragon1Multiplier = ~dragon1Multiplier;
+        dragonB.setBonusPower(complimentDragon2Multiplier * dragonB.getAttack());
+        dragonA.setBonusPower(complimentDragon1Multiplier * dragonA.getAttack());
+    }
+
+    public void countHealth(Dragon dragonA, Dragon dragonB) {
+        int DAAttack = dragonA.getAttack() * dragonA.getBonusPower() + dragonA.getAttack();
+        int DBAttack = dragonB.getAttack() * dragonB.getBonusPower() + dragonB.getAttack();
+        dragonA.setHealth(dragonA.getHealth() - DBAttack);
+        dragonB.setHealth(dragonB.getHealth() - DAAttack);
+    }
+
+    public void searchWinner(Dragon fstDng, Dragon scndDgn, int rounds) {
+        if (fstDng.getHealth() <= 0) {
+            System.out.println(scndDgn.getName() + " won, " + fstDng.getName() + " lost in " + rounds + " round.");
+        } else if (scndDgn.getHealth() <= 0) {
+            System.out.println(fstDng.getName() + " won, " + scndDgn.getName() + " lost in " + rounds + " round.");
+        } else if (fstDng.getHealth() < 0 && scndDgn.getHealth() < 0) {
+            System.out.println("Oops, we have a dead heat.");
+        }
+
+        if (rounds >= 10) {
+            if (fstDng.getHealth() > scndDgn.getHealth()) {
+                System.out.println("In 10th round " + fstDng.getName() + " won with " + fstDng.getHealth() + " HP.");
+            } else {
+                System.out.println("In 10th round " + scndDgn.getName() + " won with " + scndDgn.getHealth() + " HP.");
             }
         }
     }
-    public void countBonus(Dragon dragon1, Dragon dragon2){
-//        int tmpPower = dragon1.getPower() ^ dragon2.getPower();
-//        int complimentTmpPower = ~tmpPower;
-//        dragon2.setBonusPower(~(complimentTmpPower | dragon1.getPower()) * dragon2.getAttack());
-//        dragon1.setBonusPower(~(complimentTmpPower | dragon2.getPower()) * dragon1.getAttack());
-
-
-        int tmpPower = dragon1.getPower() ^ dragon2.getPower();
-        int complimentTmpPower = ~tmpPower;
-        int dragon2Multiplier = complimentTmpPower | dragon1.getPower();
-        int dragon1Multiplier = complimentTmpPower | dragon2.getPower();
-        int complimentDragon2Multiplier = ~dragon2Multiplier;
-        int complimentDragon1Multiplier = ~dragon1Multiplier;
-        dragon2.setBonusPower(complimentDragon2Multiplier * dragon2.getAttack());
-        dragon1.setBonusPower(complimentDragon1Multiplier * dragon1.getAttack());
-
-
-
-    }
 }
-
