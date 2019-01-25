a5e2df21b3e04d494ed3aaea707c7815fa519f19
RuslanMck <mckruslan@gmail.com>
2019-01-25 13:11:01 +0200

    2019.01.25 13:10

--- homeWork/Dragons/src/Main.java
+++ homeWork/Dragons/src/Main.java
@@ -1,16 +1,8 @@
-import model.Dragon;
+import model.UnrealTournament;
 
 public class Main {
     public static void main(String[] args) {
-        DragonCreator dragonCreator = new DragonCreator();
-        Arena arena = new Arena();
-        Dragon[] dragons = dragonCreator.createDragons();
-        for (int i = 0; i < dragons.length; i++){
-            System.out.println(dragons[i].getPower());
-        }
-
-        arena.fight(dragons[0], dragons[1]);
-
-
+        UnrealTournament tournament = new UnrealTournament();
+        tournament.startTournament();
     }
 }
