ae7f14c2576e883c7e11131ace959448c818a251
RuslanMck <mckruslan@gmail.com>
2019-02-01 19:44:11 +0200

    Add BaseDragonRider

--- homeWork/ArenaTournament/src/base/BaseDragonRider.java
+++ homeWork/ArenaTournament/src/base/BaseDragonRider.java
@@ -1,8 +1,32 @@
 package base;
 
-public class BaseDragonRider extends ArenaFighter {
+public class BaseDragonRider extends ArenaFighter implements ActionPreFight {
+
+    public int dragonsHealth;
 
     public BaseDragonRider(String name, int health, int attack, double defense) {
         super(name, health, attack, defense);
+
+
+    }
+
+    @Override
+    public void attackArenaFighter(ArenaFighter fighter) {
+        super.attackArenaFighter(fighter);
+    }
+
+    public int countAttack(BaseDragon dragon) {
+        return this.getAttack() * dragon.getAttack();
+    }
+
+    private boolean isDragonAlive(BaseDragonRider rider) {
+        if (rider.getHealth() > rider.getFullHP()) {
+            return true;
+        } else return false;
+    }
+
+    @Override
+    public void goActionPreFight() {
+
     }
 }
