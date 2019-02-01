6e1473e9309e505907f2c3701230707299c58658
RuslanMck <mckruslan@gmail.com>
2019-02-01 19:15:36 +0200

    Add byte mask

--- homeWork/ArenaTournament/src/base/BaseDragon.java
+++ homeWork/ArenaTournament/src/base/BaseDragon.java
@@ -2,25 +2,23 @@ package base;
 
 public class BaseDragon extends ArenaFighter {
 
-//    public static final int FIRE = 0b0001;
-//    public static final int EARTH = 0b0010;
-//    public static final int WATER = 0b0100;
-//    public static final int WIND = 0b1000;
-//    int power;
-//    int powers[] = {FIRE, EARTH, WATER, WIND};
-    Elements elements;
+    Elements[] elements;
 
-    public BaseDragon(String name, int health, int attack, double defense, Elements elements) {
+    public BaseDragon(String name, int health, int attack, double defense, Elements[] elements) {
         super(name, health, attack, defense);
         this.elements = elements;
     }
 
-    public int getElements() {
-        return elements.getValue();
+    public int countByteMask(){
+        int byteMask = 0;
+        for (Elements i : elements){
+            byteMask |= i.getValue();
+        }
+        return byteMask;
     }
 
     public int getPowerMultiplier(int power) {
-        int difPower = this.elements.getValue() ^ power;
+        int difPower = this.countByteMask() ^ power;
         int count = 1;
         for (Elements p : Elements.values()) {
             count += (difPower & p.getValue()) > 0 ? 1 : 0;
@@ -29,7 +27,7 @@ public class BaseDragon extends ArenaFighter {
     }
 
     public int countAttack(ArenaFighter fighter) {
-        int bonusAttack = getPowerMultiplier(fighter instanceof BaseDragon ? ((BaseDragon) fighter).getElements() : 0);
+        int bonusAttack = getPowerMultiplier(fighter instanceof BaseDragon ? ((BaseDragon) fighter).countByteMask() : 0);
         return fighter.getAttack() * bonusAttack;
     }
 
